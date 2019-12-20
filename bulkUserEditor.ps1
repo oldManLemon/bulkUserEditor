03c4d5cdd804187e8ca8c9d7d91fa6d30055cad2
Hase <ahase@intern.ahs-de.com>
2019-12-20 16:36:55 +0100

    Changed the headers in the csv for the Seks

--- bulkUserEditor.ps1
+++ bulkUserEditor.ps1
@@ -1,25 +1,26 @@
-#TODO Create script that will read the all users in STATION
-#TODO Get those users Full Names, Usernames, Emails, Phone Numbers, Description, Dep and Company
+
 #TODO RExport to CSV for editing. 
 #TODO Push edited CSV back so that updates to user details can be made
 
 
 param(
     # [Parameter(Mandatory = $true)]
-    [string] $Station
+    [string] $Station,
+    [switch] $Gather = $true,
+    [switch] $Export
 )
 
 #Avoid errors by forcing correct OU searches
 switch ($Station) {
-    "AIC" { $search = "OU=AIC,"}
-    "BER" { $search = "OU=BER,"}
+    "AIC" { $search = "OU=AIC," }
+    "BER" { $search = "OU=BER," }
     "BEr (TXL)" { $search = "OU=BER (TXL)," }
-    "BRE" { $search ="OU=BRE," }
-    "CGN" {$search =  "OU=CGN," }
-    "DUS" {$search =  "OU=DUS," }
-    "FRA" {$search =  "OU=FRA," }
-    "HAJ" {$search =  "OU=HAJ," }
-    "HAM" {$search = "OU=HAM,"  }
+    "BRE" { $search = "OU=BRE," }
+    "CGN" { $search = "OU=CGN," }
+    "DUS" { $search = "OU=DUS," }
+    "FRA" { $search = "OU=FRA," }
+    "HAJ" { $search = "OU=HAJ," }
+    "HAM" { $search = "OU=HAM," }
     "HQ" { $search = "OU=HQ," }
     "MUC" { $search = "OU=MUC," }
     "STR" { Return Write-Host "Funktioniert Nicht! Bitte Sehe Andrew" -ForegroundColor Red }
@@ -27,15 +28,29 @@ switch ($Station) {
 }
 # * Search base built here
 $base = "OU=Stations,OU=x,DC=intern,DC=ahs-de,DC=com"
-$searchbase = $search+$base
+$searchbase = $search + $base
 
-#Gather Users from the Group
-$Users = Get-ADUser -Filter * -SearchBase $searchbase -Properties samaccountname, displayName, telephoneNumber, mail, Description, Department, Company
-# $Users.samaccountname
-# $Users.displayName
-# $Users.telephoneNumber
-# $Users.mail
-# $Users.Description
-# $Users.Department
-# $Users.Company
 
+
+if ($Gather) {
+    #Gather Users from the Group
+    $Users = Get-ADUser -Filter * -SearchBase $searchbase -Properties samaccountname, displayName, telephoneNumber, mail, Description, Department, Company
+    # *This below changes the default Names as the Secerataries will edit these. Might as well make it easy to understand. 
+    # *Plus I knew not doing this would result in problems later. 
+    $Users | foreach {
+        new-object psobject -Property @{
+            Benutzer             = $_.sAMAccountName
+            "Vollstaendiger Name" = $_.displayName
+            Nummer               = $_.telephoneNumber
+            Email                = $_.mail
+            Beschreibung         = $_.description
+            Abteilung            = $_.Department
+            Betreib              = $_.Company
+        }
+
+    } | Select Benutzer, "Vollständiger Name", Nummer, Email, Beschreibung, Abteilung | Export-Csv $Station".csv" -Encoding UTF8
+}
+
+if($Export){
+
+}
